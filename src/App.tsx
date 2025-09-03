cdcbfd7c58eb7415411c33db5a939b26985fa03f
Jhefry09 <jhefrycabanillas@gmail.com>
2025-09-02 19:45:29 -0500

    borrado de archivos sobrantes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/home/page";
 import Login from "./pages/login/page";
-import Construccion from "./pages/construccion/page";
-import TestPage from "./pages/test/testPage";
 import Registro from "./pages/registro/page";
 import Dashboard from "./pages/dashboard/page.tsx";
 import Prediccion from "./pages/prediccion/page"
@@ -13,8 +11,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/construccion" element={<Construccion />} />
-        <Route path="/test" element={<TestPage />} />
         <Route path="/registro" element={<Registro />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/prediccion" element={<Prediccion/>} />
